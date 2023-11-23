c109cb92ab5db2aea9f95b0e8c8e6cea51a0c717
leonardo <leonardocaixeta07@gmail.com>
2023-11-23 16:25:09 -0300

    feat: method read and get_unique_job_types

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,3 +1,4 @@
+import csv
 from typing import List, Dict
 
 
@@ -5,11 +6,23 @@ class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        with open(path, newline='', encoding='utf-8') as file:
+            reader = csv.DictReader(file)
+            self.jobs_list = list(reader)
 
     def get_unique_job_types(self) -> List[str]:
-        pass
+        job_types = []
+        for jobs_list in self.jobs_list:
+            if not jobs_list['job_type'] == '' and (
+                not job_types.__contains__(jobs_list['job_type'])
+            ):
+                job_types.append(jobs_list['job_type'])
+
+        return job_types
 
     def filter_by_multiple_criteria(self) -> List[dict]:
         pass
+
+
+process = ProcessJobs()
--- src/pre_built/counter.py
+++ src/pre_built/counter.py
@@ -3,6 +3,3 @@ def count_ocurrences(path: str, word: str) -> int:
     read_data = file.read()
     word_count = read_data.lower().count(word.lower())
     return word_count
-
-
-print(count_ocurrences('data/jobs.csv', 'word'))
